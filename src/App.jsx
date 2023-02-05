e77591da7069471ede6a5ffa12d212cccc38b0ec
Andre Grassi <andregrassi.j@gmail.com>
2023-02-05 16:38:22 -0300

    Use only one state for the list and key. Also manage a workaround to the bug of no id and no key.

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,62 @@
-import AddGoal  from "./components/AddGoal";
-import { useState } from "react";
+import AddGoal from './components/AddGoal'
+import { useState } from 'react'
 
 function App() {
   // Array that contains the goals
-  const [list, listHelper] = useState([])
+  const [list, setList] = useState({
+    item: [],
+    key: [0]
+  })
 
-  // Array that contains the keys
-  // let key = []
-  const [key, keyHelper] = useState([])
-  
   // Function that adds goals to the list
   function addGoal(title) {
-    // key.push(Math.round(Math.random()*1000))
-    keyHelper((prevState) => [...prevState, Math.round(Math.random()*1000)])
-    /*
-    Na real esse código ta sendo gerado sim na primeira passagem,
-    mas como é assíncrono não da tempo do keyhelper gerar uma nova chave e
-    o console.log já vai printando e acaba printando o estado anterior do keyhelper
-    */
-    console.log(key) 
-    console.log(key.at(-1))
-    // O primeiro item da lista ta sempre ficando sem id e acho que sem key também
-    listHelper((prevState) => 
-      [
-        ...prevState,
-        <li onClick={removeGoal} id={key.at(-1)} key={key.at(-1)} className="list-group-item">{title}</li>
-      ])
+    setList(prevState => {
+      return {
+        // Known bug: if the key property is not initialized with 0, then the first item won't have neither id nor key
+        key: [...prevState.key, Math.round(Math.random() * 1000)],
+        item: [
+          ...prevState.item,
+          <li
+            onClick={removeGoal}
+            id={list.key[list.key.length - 1]}
+            key={list.key[list.key.length - 1]}
+            className="list-group-item"
+          >
+            {title}
+          </li>
+        ]
+      }
+    })
   }
 
   function removeGoal(event) {
     const item = event.target
     // console.log(list[0].key)
     // console.log(item.id)
+    /*
     for (let i = 0; i < list.length; i++) {
       console.log(list[i].key, item.id)
-      if (list[i].key == item.id)
-      {
+      if (list[i].key == item.id) {
         list.splice(i, 1)
         listHelper(list)
       }
     }
+    */
   }
-  
+
   return (
     <div className="App d-flex flex-column align-items-center">
       <header className="my-3">
-        <h1 className='display-1 fw-bold'>Goal list</h1>
-        <h2 className='h1'>achieve your goals.</h2>
+        <h1 className="display-1 fw-bold">Goal list</h1>
+        <h2 className="h1">achieve your goals.</h2>
       </header>
       <main className="d-flex flex-column align-items-center mt-3">
-        <AddGoal submitGoal={addGoal}/> {/*submitGoal is used for passing data from child to parend */}
-        <ul className="list-group">{list}</ul>
+        {/*submitGoal is used for passing data from child to parend */}
+        <AddGoal submitGoal={addGoal} />
+        <ul className="list-group">{list.item}</ul>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
